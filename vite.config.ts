97174ee7f67d971f23b3f4c8b0711e0021f4f1b9
jcrosby <john.crosby@mlb.com>
2025-03-12 08:54:08 -0400

    chore: swaps @vercel/react-router/vercel with vite-plugin-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { vercelPreset } from '@vercel/react-router/vite';
+import vercel from 'vite-plugin-vercel';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [vercelPreset(), tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), vercel()],
 });
