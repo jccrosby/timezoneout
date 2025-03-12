5e2f9792559d3b189c2730ea847d864a07934e3b
jcrosby <john.crosby@mlb.com>
2025-03-12 08:56:21 -0400

    fix: sequence vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), vercel()],
+    plugins: [vercel(), tailwindcss(), reactRouter(), tsconfigPaths()],
 });
